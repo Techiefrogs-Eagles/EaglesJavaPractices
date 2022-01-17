7573ea2aefb129041fc651fd89ac1d68d9b6f043
lakshmansastry <97865321+lakshmansastry@users.noreply.github.com>
2022-01-17 18:42:42 +0530

    Update App.java

--- lakshman/src/App.java
+++ lakshman/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println(args[0] + " " + args[1] + " " + args[2]);
+     //   System.out.println(args[0] + " " + args[1] + " " + args[2]);
     }
 }
