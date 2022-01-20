693491f5ef816f1399d688a0453a704f7e3ee65d
chaitanya9866 <18555a0230@gmail.com>
2022-01-20 08:09:37 +0530

    classintancedemo

---
+++ chaitanya/src/classintancedemo.java
@@ -0,0 +1,20 @@
+public class classintancedemo {
+
+    int i = 10;
+    String str ="Hello";
+
+    public static void main(String[] args) {
+        int a = 17;
+
+        classintancedemo classobject = new classintancedemo() ;
+
+        classobject.str = "team";
+
+        classintancedemo cobject1 = new classintancedemo();
+        cobject1.str = "eagles";
+
+    System.out.println(classobject.i+"  "+classobject.str);
+
+    }
+    
+}
