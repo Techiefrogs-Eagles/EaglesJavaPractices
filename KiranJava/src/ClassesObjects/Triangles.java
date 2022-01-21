0e3dafdc7ecbb7dfa0b8a96333b4529e54887e7c
S-KiranKumarReddy <kkreddy443@gmail.com>
2022-01-21 23:22:22 +0530

    Kiran commit

--- KiranJava/src/ClassesObjects/Triangles.java
+++ KiranJava/src/ClassesObjects/Triangles.java
@@ -1,3 +1,23 @@
-public class Triangles {
+
+class Triangle{
+  int a,b,c;
+  public double getArea(){
+    double s = (a+b+c)/2.0;
     
+    return Math.pow((s*(s-a)*(s-b)*(s-c)),.5);
+  }
+  public double getPerimeter(){
+    return (a+b+c)/2.0;
+  }
 }
+
+public class Triangles{
+  public static void main(String[] args){
+    Triangle t = new Triangle();
+    t.a = 3;
+    t.b = 4;
+    t.c = 5;
+    System.out.println(t.getArea());
+    System.out.println(t.getPerimeter());
+  }
+}	
