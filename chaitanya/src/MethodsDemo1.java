0f87c3f7582af2a67459934f5a95d45102224fa1
chaitanya9866 <18555a0230@gmail.com>
2022-01-27 21:32:15 +0530

    MethodsDemo

---
+++ chaitanya/src/MethodsDemo1.java
@@ -0,0 +1,35 @@
+public class MethodsDemo1 {
+    public static void main(String[] args) { 
+        Methodsdemo methoddemo = new Methodsdemo();
+        int sum = methoddemo.sum(5,10);
+        System.out.println("The sum of 2 values is  "+sum);
+    }
+    
+    void display(){
+        System.out.println("user defined");
+    
+    }
+
+    int sum( int a, int b){
+        System.out.println("my sum value");
+        return a+b;
+    }
+
+    boolean metch(){
+        return true;
+    }
+
+    String mymethod(String name){
+        if(name == "Satya")
+           return "ABC";
+        else{ 
+            System.out.println("hi");
+            //return name;
+        }
+        return name;
+    
+    }
+}
+
+    
+
