14bcd585603d32046afd3ce8afbcc512401d33c3
visalaNandigam <nvisala536@gmail.com>
2022-01-29 07:47:29 -0500

    visala commit

---
+++ visala/src/inheritance/Chaild1.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Chaild1 extends Parent {
+    public static void main(String[] args) {
+        Chaild1 cobj1=new Chaild1();
+        cobj1.display();
+        cobj1.display1();
+        System.out.println(cobj1.sum(2,3));
+
+        
+       System.out.println( cobj1.mul());
+
+        
+    }
+    
+}
--- visala/src/inheritance/Parent.java
+++ visala/src/inheritance/Parent.java
@@ -1,3 +1,5 @@
+package inheritance;
+
 public class Parent extends Grandparent {
     
      void display1()
