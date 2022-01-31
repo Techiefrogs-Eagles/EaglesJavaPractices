8a2aacdb9683851b0bc0d504153f9c4d67950093
lakshmansastry <sastry709@gmail.com>
2022-01-31 16:10:53 +0530

    assignment commit

--- lakshman/src/practice/subrahmanyamPC.java
+++ lakshman/src/practice/subrahmanyamPC.java
@@ -3,6 +3,12 @@ package practice;
 public class subrahmanyamPC extends murthyGP
 {
     int money1 = 500000;
+
+    subrahmanyamPC()
+    {
+        super(10,"hello");
+    }
+    
     public static void main(String[] args) 
     {
         subrahmanyamPC obj = new subrahmanyamPC();
