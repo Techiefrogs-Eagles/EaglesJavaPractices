2ba45b2e19acd537efca9344bf35e58e545bcc34
visalaNandigam <nvisala536@gmail.com>
2022-02-02 23:34:15 -0500

    Abstract

---
+++ visala/src/Abstract/Abstractdemo3.java
@@ -0,0 +1,28 @@
+package Abstract;
+
+public class Abstractdemo3 extends Abstractdemo1{
+    void displayA()
+    {
+        System.out.println("This is displayA abstract");
+
+    }
+  void display1()
+  {
+      System.out.println("This is child1 display");
+  }
+  //void finaldisplay()
+  //{
+
+  //}
+    public static void main(String[] args) {
+        Abstractdemo3 ab3=new Abstractdemo3();
+        ab3.displayA();
+        ab3.display();
+        ab3.display1();
+        ab3.newdisplay();
+        ab3.finaldisplay();
+        Abstractdemo.staticdisplay();
+
+    }
+    
+}
