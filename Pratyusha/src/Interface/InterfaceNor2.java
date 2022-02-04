a245a301f08a94ce7b15fa4b05f123d233f3e64e
Pratyusha32 <pratyushasmylee@gmail.com>
2022-02-04 17:19:37 +0530

    new

---
+++ Pratyusha/src/Interface/InterfaceNor2.java
@@ -0,0 +1,59 @@
+package Interface;
+
+ abstract public class InterfaceNor2 extends InterfaceNor1 {
+
+    
+    public void display2() {
+        
+        System.out.println("in inter3 display2 ");
+    }
+
+    
+    public String display3() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void display() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String display1() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void display51() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void String52() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void display41() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void display32() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    void displayN11() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ Pratyusha/src/Interface/InterfaceNor3.java
@@ -0,0 +1,5 @@
+package Interface;
+
+public class InterfaceNor3 extends interfaceNor2 implements interface4
+    
+}
