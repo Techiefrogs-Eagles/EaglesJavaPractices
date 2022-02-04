2e3fdf2cc03fa9f483c6811d45ca6c9850a50f43
visalaNandigam <nvisala536@gmail.com>
2022-02-03 21:30:50 -0500

    comit Assignment

---
+++ visala/src/AbstractAssignment/Progrsm7.java
@@ -0,0 +1,20 @@
+package AbstractAssignment;
+
+public class Progrsm7 {
+    
+}
+abstract class Shape
+{
+    protected String colour="red";
+    protected Boolean filed=true;
+
+    void Shape()
+    {
+
+    }
+    void Shape(String colour,Boolean filed)
+    {
+        
+    }
+
+}
