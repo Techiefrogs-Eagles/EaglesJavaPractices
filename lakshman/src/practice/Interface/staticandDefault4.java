ca40d401cce9c4bbfd8b9d4f7ed4ef057a2df3e2
lakshmansastry <sastry709@gmail.com>
2022-02-07 23:03:40 +0530

    ASSIGNMENT AND PRACTICE

---
+++ lakshman/src/practice/Interface/staticandDefault4.java
@@ -0,0 +1,13 @@
+package practice.Interface;
+
+public interface staticandDefault4 
+{
+    int a = 10;
+    //static { a=10; }
+    void fan();
+    
+    default void normalmet1(){
+        System.out.println( "in normet1 default method 1");
+    }
+
+}
---
+++ lakshman/src/practice/Interface/staticanddefault2.java
@@ -0,0 +1,13 @@
+package practice.Interface;
+
+public interface staticanddefault2{
+
+    void light();
+    default void normalmet2(){
+        System.out.println( "in normet2 default method 2");
+    }
+
+    static void king2(){
+        System.out.println("i king2 method");
+    }
+}
