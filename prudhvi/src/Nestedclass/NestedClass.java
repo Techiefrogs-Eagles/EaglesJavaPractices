f66c9b1a27bd4f30ba37a668c2d307836f800af9
raparla45 <prudhvi.raparla45@gmail.com>
2022-02-09 17:25:38 +0530

    prudhvi commit

---
+++ prudhvi/src/Nestedclass/NestedClass.java
@@ -0,0 +1,43 @@
+public class NestedClass {
+    int i = 10;
+    String name = "john";
+
+    void display(){
+        System.out.println("in dispaly");
+    }
+
+    NestedClass(){
+        System.out.println("in constructor");
+
+    }
+
+    class Myinnerclass{
+         int k = 30;
+
+         void innerdisplay(){
+             System.out.println("in inner display");
+
+         }
+         Myinnerclass(){
+             System.out.println(" in inner constructor");
+         }
+
+    }
+    
+    public static void main(String[] args) {
+
+        NestedClass outerobj = new NestedClass();
+        Myinnerclass innerobj = outerobj.new Myinnerclass();
+        innerobj.innerdisplay();
+
+        
+    
+        
+    }
+
+    
+    
+    
+
+    
+}
