c8dd1aedab2e50a5f8ca2c47eda896aabcf5406a
lakshmansastry <sastry709@gmail.com>
2022-02-16 00:33:53 +0530

    assignment

---
+++ lakshman/src/Assignment8/Program17.java
@@ -0,0 +1,14 @@
+package Assignment8;
+
+public class Program17 
+{
+    public static void main(String[] args) 
+    {
+        String str = "lakshman";
+        int a = str.length();
+        for (int i = 1; i <= a; i++) 
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
---
+++ lakshman/src/Assignment8/program3.java
@@ -0,0 +1,7 @@
+package Assignment8;
+
+public class program3 {
+    public static void main(String[] args) {
+      //  String str= "second/jan/two thousand twenty";
+    }
+}
