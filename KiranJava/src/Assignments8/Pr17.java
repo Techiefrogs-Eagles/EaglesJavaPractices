7feb4a00706af62e11cd7a061020865dfe67d2e7
S-KiranKumarReddy <kkreddy443@gmail.com>
2022-02-17 23:36:32 +0530

    Kiran Commit

---
+++ KiranJava/src/Assignments8/Pr17.java
@@ -0,0 +1,13 @@
+package Assignments8;
+
+public class Pr17 {
+    public static void main(String[] args) 
+    {
+        String str = "char is already present";
+        int a = str.length();
+        for (int i = 0; i <= a; i++) 
+        {
+            System.out.print(str.charAt(i)+" ");
+        }
+    }
+}
---
+++ KiranJava/src/Assignments8/Pr2.java
@@ -0,0 +1,9 @@
+package Assignments8;
+
+public class Pr2 {
+    public static void main(String[] args) {
+        String s="asdf";
+    String d="bsdf";
+    System.out.println(s.compareToIgnoreCase(d));
+    }
+}
