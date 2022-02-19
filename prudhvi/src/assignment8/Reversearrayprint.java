39fe50a28fa50f91182e20ddb433694ef2312bae
raparla45 <prudhvi.raparla45@gmail.com>
2022-02-19 12:30:55 +0530

    prudhvicommit

---
+++ prudhvi/src/assignment8/Reversearrayprint.java
@@ -0,0 +1,18 @@
+package assignment8;
+
+public class Reversearrayprint {
+    public static void main(String[] args) {
+        
+        int arr[] = new int[6];
+
+        arr[0] = 20;
+        arr[1] = 7;
+        arr[2] = 90;
+        arr[4] = 45;
+        for(int i=5;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+
+    }
+    
+}
