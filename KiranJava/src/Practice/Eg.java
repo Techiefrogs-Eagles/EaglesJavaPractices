2e510a09c2f6e5cddd0d97d437997fe88a5f6779
raparla45 <prudhvi.raparla45@gmail.com>
2022-02-19 11:16:59 +0530

    commit

---
+++ KiranJava/src/Practice/Eg.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Eg {
+    public static void main(String[] args) {
+        Encdemo g=new Encdemo();
+        g.setI(456);
+       int f=g.getI();
+       System.out.println(f);
+    }
+}
