71bf90ba146dc3da9bd54fd067ae2324f74854ba
visalaNandigam <nvisala536@gmail.com>
2022-02-20 21:35:50 -0500

    comit

---
+++ visala/src/ArrayAssignment/Program41.java
@@ -0,0 +1,13 @@
+package src.ArrayAssignment;
+
+public class Program41 {
+    public static void main(String[] args) {
+        String Sarr[]={"Hello"};
+        String Sarr1[]={"Hi"};
+String Strr3[]=new String[Sarr.length+Sarr1.length];
+System.out.println((Sarr.length)concat(Sarr1.length));
+for (String s : Strr3) {
+    System.out.println(s+" ");
+}
+    }
+}
