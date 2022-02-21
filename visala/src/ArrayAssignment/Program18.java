82fd70d9a033bff16dd9c741e2b80c5b2182540a
visalaNandigam <nvisala536@gmail.com>
2022-02-20 21:36:22 -0500

    comit

--- visala/src/ArrayAssignment/Program18.java
+++ visala/src/ArrayAssignment/Program18.java
@@ -2,9 +2,10 @@ package src.ArrayAssignment;
 
 import java.util.Arrays;
 
-public class Program16 {
+public class Program18 {
+    
     public static void main(String[] args) {
-        int[] array = {15,29,63,8};
+        int[] array = {15,29,63,8,3};
         Arrays.sort(array);
         System.out.print("ascending order of array    ");
         for (int c : array) 
@@ -12,7 +13,9 @@ public class Program16 {
             System.out.print(c+" ");
         }
         System.out.println();
-        System.out.println("3rd largest number = "+array[array.length-2]);
+        System.out.println("2nd smallest number  "+array[1]);
     }
     
 }
+    
+
