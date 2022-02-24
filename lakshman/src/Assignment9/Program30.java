84c1be234e8f1cc145e20375da86cc86f7067d94
lakshmansastry <sastry709@gmail.com>
2022-02-24 08:15:39 +0530

    assignment  final

---
+++ lakshman/src/Assignment9/Program30.java
@@ -0,0 +1,30 @@
+package Assignment9;
+
+public class Program30 {
+    public static void main(String[] args) 
+    {
+        int a [][] = {{1,2,3},{1,2,3},{1,2,3}};
+        int  SumOfRows = 0;
+        int SumOfColms = 0;
+
+        for(int i = 0; i < a.length ;i++)
+        { 
+            SumOfRows = 0;
+           for(int j = 0; j < a[i].length;j++)
+           {
+                SumOfRows = SumOfRows+ a[i][j];
+           }
+           System.out.println("Sum of rows: "+ SumOfRows);
+        }
+
+       for(int i = 0; i < a.length ; i++)
+       {
+           SumOfColms = 0;
+           for(int j = 0; j< a[i].length;j++)
+           {
+              SumOfColms = SumOfColms + a[j][i];
+           }
+           System.out.println("Sum of colums: "+ SumOfColms);
+        }
+    }
+}
