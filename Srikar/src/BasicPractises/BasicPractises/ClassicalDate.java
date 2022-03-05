df117ad03fc79fc6b5519d48966dc10c4d721658
Srikar4343 <margapurisreekar@gmail.com>
2022-03-05 18:10:31 +0530

    Collections Practise

---
+++ Srikar/src/BasicPractises/BasicPractises/ClassicalDate.java
@@ -0,0 +1,11 @@
+package BasicPractises.BasicPractises.Arrays;
+
+import java.sql.Date;
+
+public class ClassicalDate {
+    public static void main(String[] args) {
+        Date d1 = new java.util.Date();
+        System.out.println(d1);
+    }
+     
+}
