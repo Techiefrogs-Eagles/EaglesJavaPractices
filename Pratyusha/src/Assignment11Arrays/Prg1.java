ae48e9376a6353bf954a072f3fd56a15f396fee0
Pratyusha32 <pratyushasmylee@gmail.com>
2022-03-07 08:14:09 +0530

    commit

---
+++ Pratyusha/src/Assignment11Arrays/Prg1.java
@@ -0,0 +1,24 @@
+package Assignment11Arrays;
+
+import java.util.ArrayList;
+
+public class Prg1 
+{
+    public static void main(String[] args) 
+    {
+    ArrayList<String> Colour = new ArrayList<String>();
+    Colour.add("Pink");
+    Colour.add("Blue");
+    Colour.add("Black");
+    Colour.add("Violet");
+    Colour.add("Purpule");
+
+    for(String str : Colour)
+    {
+        System.out.println(str);
+    }
+
+
+
+}
+}
