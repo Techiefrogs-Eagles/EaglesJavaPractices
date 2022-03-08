ef3842f82f2320d2db830f39249ad9136abbcc7e
visalaNandigam <nvisala536@gmail.com>
2022-03-08 13:03:37 -0500

    comit

---
+++ visala/src/CollectionAssignment/LinkedlistAssignment/Program19.java
@@ -0,0 +1,22 @@
+package src.CollectionAssignment.LinkedlistAssignment;
+
+import java.util.LinkedList;
+
+public class Program19 {
+    public static void main(String[] args) {
+        LinkedList<String> Link=new LinkedList<>();
+        Link.add("red");
+        Link.add("Blue");
+        Link.add("green");
+        Link.add("pink"); 
+        Link.add("black");
+        Link.add("gray");
+        Link.remove(0);
+        
+      for (String string : Link) {
+          
+           System.out.println(string);
+      }
+
+    }
+}
