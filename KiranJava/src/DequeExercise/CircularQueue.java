7ae9b775c491a44b7e1a48d41dbd57211a7fadbf
S-KiranKumarReddy <kkreddy443@gmail.com>
2022-03-10 08:01:32 +0530

    Hashset

--- KiranJava/src/DequeExercise/CircularQueue.java
+++ KiranJava/src/DequeExercise/CircularQueue.java
@@ -8,11 +8,41 @@ public class CircularQueue {
     int size; 
     public CircularQueue(int size)  
     {  
-      
-        // The arr array is initialized with the MAX value defined earlier  
+       // The arr array is initialized with the MAX value defined earlier  
         arr = new int[MAX];  
         front = -1;  
         rear = 0;  
         this.size = size;  
     }   
+    boolean isFull()  
+    {  
+        return ((front == 0 && rear == size-1)||  
+            front == rear+1);  
+    }  
+    boolean isEmpty ()  
+    {  
+        return (front == -1);  
+    }  
+    void insertfront(int key)  
+    {  
+        // check whether Deque if full or not  
+        if (isFull())  
+        {  
+            System.out.println("Overflow");  
+            return;  
+        }  
+    if (front == -1)  
+        {  
+            front = 0;  
+            rear = 0;  
+        } 
+        else if (front == 0)  
+        front = size - 1 ;  
+
+    else // decrement front end by '1'  
+        front = front-1;  
+
+    // insert current element into Deque  
+    arr[front] = key ;  
+}   
 }
